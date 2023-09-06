7ea719ecbf0d29e292cd32194f7c7d201a322d22
Oladipin <>
2023-09-06 14:47:54 +0000

    Add event listeners for DOM and button clicks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+// Event listeners for DOM to finish loading and to get button element
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let playerChoice = this.getAttribute("data-type");
+            console.log("You clicked a game choice");;
+
+        });
+    };
+
+});
 function runGame() {
 
 }
